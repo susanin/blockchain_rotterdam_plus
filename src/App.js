6d34fdf1b876ddedabcb99d8059adad67e8e07a9
Kirill Vergun <o-nix@users.noreply.github.com>
2017-06-11 16:26:42 +0300

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
+import * as Web3 from 'web3';
 
 const notCompiledContract = import('./SignDocument.js');
 
 let SignDocument;
-
-const web3 = window.web3;
+let web3;
 
 class Person {
   name;
@@ -20,9 +19,9 @@ class Person {
 }
 
 const approvers = [
-  new Person('Ceplialo', '0x0805d6E32a66ACd51cD00168B0989872785b278b'),
-  new Person('Hvatalov', '0x12987A48A6C4c4aA32f2c79bee8A162105F8a781'),
-  new Person('Grabchenko', '0x3ad4a4E100c5dc8a58C848dD9856631207C225dA')
+  new Person('Emanoil Manolescu', '0x0805d6E32a66ACd51cD00168B0989872785b278b'),
+  new Person('David Șerban', '0x12987A48A6C4c4aA32f2c79bee8A162105F8a781'),
+  new Person('Valerian Neagu', '0x3ad4a4E100c5dc8a58C848dD9856631207C225dA')
 ];
 
 class App extends Component {
@@ -31,40 +30,49 @@ class App extends Component {
     this.state = {
       currentApprovers: []
     };
+  }
+
+  componentDidMount() {
+    if (typeof window.web3 === 'undefined') {
+      web3 = new Web3(new Web3.providers.HttpProvider("http://40.118.96.33:8545"));
+    } else {
+      web3 = window.web3;
+    }
 
     SignDocument = web3.eth.contract([notCompiledContract]);
   }
 
-  componentDidMount() {
-  }
-
   sendContract() {
-    SignDocument.new(this.state.currentApprovers.map((a) => a.id), {}, (a, b,c ,d) => console.log(a,b,c,d))
+    SignDocument.new(this.state.currentApprovers.map((a) => a.id), {from: '0xBf800F94E82438943c967Fe00C8a549381a650Ac'}, (a, b,c ,d) => console.log(a,b,c,d))
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src="http://www.uspa.gov.ua/images/port_logo-ua.png" className="" alt="logo" />
+          <h2>Контракты</h2>
         </div>
         <div className="App-intro">
-          {
-            approvers.map((a) => {
-              return <div key={a.id}>{a.name} <button onClick={e => this.setState({...this.state, currentApprovers: (this.state.currentApprovers.indexOf(a) === -1) ? this.state.currentApprovers.concat(a) : this.state.currentApprovers})}>+</button></div>;
-            })
-          }
+          <div className="Contract-holder">
+            <div className="Contract">
+              {
+                approvers.map((a) => {
+                  return <div className="Approvers Approvers-available" key={a.id}>{a.name} <button onClick={e => this.setState({...this.state, currentApprovers: (this.state.currentApprovers.indexOf(a) === -1) ? this.state.currentApprovers.concat(a) : this.state.currentApprovers})}>+</button></div>;
+                })
+              }
 
-          <p>Approvers:</p>
+              <p>Выбраны:</p>
 
-          <div>
-            {
-              this.state.currentApprovers.map(a => <div key={a.id}>{a.name}</div>)
-            }
+              <div>
+                {
+                  this.state.currentApprovers.map(a => <div className="Approvers Approvers-selected" key={a.id}>{a.name}</div>)
+                }
+              </div>
+
+              <button onClick={e => this.sendContract()}>Отправить на подпись</button>
+            </div>
           </div>
-
-          <button onClick={e => this.sendContract()}>Send contract</button>
         </div>
       </div>
     );
